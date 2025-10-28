d204d13cd42b38373d640b7956072827d8be7e35
Nasir Crownie <ncrownie@andrew.cmu.edu>
2025-10-28 21:40:45 +0000

    Added in nullity checks

--- test/api.js
+++ test/api.js
@@ -523,6 +523,9 @@ describe('API', async () => {
 				});
 
 				it('response status code should match one of the schema defined responses', () => {
+					if (!result || !result.response) {
+						assert.fail('Previous test failed to set result - cannot validate response status');
+					}
 					// HACK: allow HTTP 418 I am a teapot, for now   👇
 					const { responses } = context[method];
 					assert(
@@ -534,6 +537,11 @@ describe('API', async () => {
 
 				// Recursively iterate through schema properties, comparing type
 				it('response body should match schema definition', () => {
+					if (!result || !result.response) {
+						assert.fail('Previous test failed to set result - cannot validate response body');
+						return;
+					}
+
 					const http302 = context[method].responses['302'];
 					if (http302 && result.response.statusCode === 302) {
 						// Compare headers instead
