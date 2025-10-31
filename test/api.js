fa642e016497c4f769036581dbb32af2d06f5c44
Sarah Cross <sarahcro@andrew.cmu.edu>
2025-10-31 17:33:20 +0000

    Cherry-pick: perf: updated to sync with bugfix + pass tests

--- test/api.js
+++ test/api.js
@@ -692,6 +692,11 @@ describe('API', async () => {
 				return;
 			}
 
+<<<<<<< HEAD
 			assert(schema[prop], `"${prop}" was found in response, but is not defined in schema (path: ${method} ${path}, context: ${context}), SCHEMA ${JSON.stringify(schema)}`);});
+=======
+			assert(schema[prop], `"${prop}" was found in response, but is not defined in schema (path: ${method} ${path}, context: ${context}), SCHEMA ${JSON.stringify(response)}`);
+		});
+>>>>>>> 3199cac60a (perf: updated to sync with bugfix + pass tests)
 	}
 });
