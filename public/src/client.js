a0989825ae91dc32ec5d97597f3ac76791d10c6c
Lena Poshni <lposhni@andrew.cmu.edu>
2025-11-11 18:08:15 +0000

    public/src/client.js edits - accidentially made edits to it

--- public/src/client.js
+++ public/src/client.js
@@ -1,2 +1,9 @@
-const name = 'Lena'  
-console.log(name)
+'use strict';
+
+require('./app');
+
+// scripts-client.js is generated during build, it contains javascript files
+// from plugins that add files to "scripts" block in plugin.json
+require('../scripts-client');
+
+app.onDomReady();
